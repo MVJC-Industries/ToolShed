f94ac808f589a6ae41893ad67a448db6eede1924
mccormsy <mmccorms10@gmail.com>
2023-11-22 17:56:19 -0800

    save and push app.test.js

--- tests/components/App.test.js
+++ tests/components/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../src/components/App';
 import '@testing-library/jest-dom'
+
 describe('App component', () => {
   test('renders "Hello world!"', () => {
     render(<App />);
