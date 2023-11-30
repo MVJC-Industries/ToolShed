8751d27ca411951d5ba6b155c600c7b56beef921
jayachankura <jayachankura@gmail.com>
2023-11-30 10:55:51 -0500

    setup basic layout of reservationCard component, about to start adding styling and functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import ReservationForm from "./ReservationForm.jsx";
+import ReservationCard from "./ReservationCard.jsx";
 
 const Login = () => {
   return (
     <div>
-      <p className="text-3xl font-bold underline">This is our login page</p>
-      <ReservationForm />
+      <p className="text-3xl font-bold underline border-2 h-full ">
+        This is our login page
+      </p>
+      <ReservationCard />
     </div>
   );
 };
--- src/components/ReservationCard.jsx
+++ src/components/ReservationCard.jsx
@@ -4,6 +4,46 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { Button, TextField, Box } from "@mui/material";
 import axios from "axios";
 
-const ReservationCard = () => {};
+// setup real data from database later
+const dummyData = {
+  title: "Milwaukee Hammer Drill M18",
+  location: 32601,
+  qty: 3,
+  pick_up_date: dayjs().toString(),
+  drop_off_date: dayjs().toString(),
+};
+
+// stretch, can just use zipcode for now
+const calculateDistanceAway = () => {
+  //use google matrix api for this
+};
+
+const edit = () => {};
+
+const submit = () => {};
+
+const ReservationCard = () => {
+  return (
+    <div className="flex flex-row w-2/5 border-2 border-rose-500">
+      <div className="flex flex-col pt-4 pl-6">
+        <img
+          className=" w-64 border-black border-2 rounded-2xl"
+          src="https://images.thdstatic.com/productImages/d7b41ac4-7a1f-4283-baf6-e85ac5e92aad/svn/milwaukee-hammer-drills-2904-20-64_1000.jpg"
+        ></img>
+        <title className="flex w-64 pl-6 pt-2 ">{dummyData.title}</title>
+        <div className="italic flex w-64 pl-6 pt-1 pb-4">
+          <p>located in {dummyData.location}</p>
+          <p className="pl-12">qty:{dummyData.qty}</p>
+        </div>
+      </div>
+      <div className="flex flex-col w-full border-black border-2 justify-center space-y-32 text-lg">
+        <p>Picked up at: {dummyData.pick_up_date}</p>
+        <p>Return by: {dummyData.drop_off_date}</p>
+      </div>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  );
+};
 
 export default ReservationCard;
